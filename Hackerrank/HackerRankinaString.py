af0683e3c8b975550e28c4746ab9c1e598cdb55a
Sudheer <singampallisudheer@gmail.com>
2019-01-07 04:07:32 +0900

    hacker rank in string

---
+++ Hackerrank/HackerRankinaString.py
@@ -0,0 +1,85 @@
+#!/bin/python3
+'''
+We say that a string contains the word hackerrank if a subsequence of its characters spell the word hackerrank. For example, if string  it does contain hackerrank, but  does not. In the second case, the second r is missing. If we reorder the first string as , it no longer contains the subsequence due to ordering.
+
+More formally, let  be the respective indices of h, a, c, k, e, r, r, a, n, k in string . If  is true, then  contains hackerrank.
+
+For each query, print YES on a new line if the string contains hackerrank, otherwise, print NO.
+
+Function Description
+
+Complete the hackerrankInString function in the editor below. It must return YES or NO.
+
+hackerrankInString has the following parameter(s):
+
+s: a string
+Input Format
+
+The first line contains an integer , the number of queries. 
+Each of the next  lines contains a single query string .
+
+Constraints
+
+Output Format
+
+For each query, print YES on a new line if  contains hackerrank, otherwise, print NO.
+
+Sample Input 0
+
+2
+hereiamstackerrank
+hackerworld
+Sample Output 0
+
+YES
+NO
+Explanation 0
+
+We perform the following  queries:
+
+ 
+The characters of hackerrank are bolded in the string above. Because the string contains all the characters in hackerrank in the same exact order as they appear in hackerrank, we print YES on a new line.
+ does not contain the last three characters of hackerrank, so we print NO on a new line.
+Sample Input 1
+
+2
+hhaacckkekraraannk
+rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt
+Sample Output 1
+
+YES
+NO
+
+'''
+import math
+import os
+import random
+import re
+import sys
+
+# astr = hackerrank
+# s    = rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktajjt
+# s    = hhaacckkekraraannk
+# Complete the hackerrankInString function below.
+def hackerrankInString(s):
+    astr = 'hackerrank'
+    i = j = 0
+    while ((i in range(len(astr))) and (j in range(len(s)))):
+        if astr[i] == s[j]:
+            i += 1
+            j += 1
+        else:
+            j += 1
+    if i == len(astr) and j <= len(s):
+        return 'YES'
+    else:
+        return 'NO' 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input())
+    for q_itr in range(q):
+        s = input()
+        result = hackerrankInString(s)
+        fptr.write(result + '\n')
+    fptr.close()
